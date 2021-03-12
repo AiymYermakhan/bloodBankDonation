84e508fabf1abb62d7b859a37a8053fb06ad0b44
AiymYermakhan <79748164+AiymYermakhan@users.noreply.github.com>
2021-03-12 15:38:13 +0600

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,37 @@
+package com.company.Database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+
+    private static DatabaseConnection instance;
+    private Connection connection;
+    private String url = "jdbc:postgresql://localhost:2002/mydb";
+    private String username = "postgres";
+    private String password = "Aiym2002.";
+
+    private DatabaseConnection() throws SQLException {
+        try {
+            Class.forName("org.postgresql.Driver");
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static DatabaseConnection getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new DatabaseConnection();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new DatabaseConnection();
+        }
+
+        return instance;
+    }
+}
