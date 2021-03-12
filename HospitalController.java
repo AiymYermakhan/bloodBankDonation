1e5f5badd040b2994e9c989e14cc824d626054cc
AiymYermakhan <79748164+AiymYermakhan@users.noreply.github.com>
2021-03-12 15:30:02 +0600

    Add files via upload

---
+++ HospitalController.java
@@ -0,0 +1,15 @@
+package com.company.Controllers;
+
+import com.company.Repositories.HospitalRepository;
+
+public class HospitalController implements HospitalRepository {
+    @Override
+    public void getName() {
+
+    }
+
+    @Override
+    public String getAddress(String hospitalName) {
+        return null;
+    }
+}
